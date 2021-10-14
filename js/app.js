9a883c3f8a4c2de399de41ff265d54be08a98c4b
Swolen <Kille250@gmail.com>
2021-10-14 21:31:37 +0200

    Added Music Playback and made some changed on the particle effects.

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ particlesJS('particles-js',
       },
       "line_linked": {
         "enable": true,
-        "distance": 150,
+        "distance": 80,
         "color": "#ffffff",
         "opacity": 0.4,
         "width": 1
